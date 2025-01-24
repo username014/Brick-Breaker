2aea3aa2a4c73ad9ea9755eaa844320ec812237a
username014 <72132264+username014@users.noreply.github.com>
2025-01-24 12:28:16 +0300

    Delete includes/includes directory

--- includes/includes/helper_functions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "blocks.h"
-#ifndef HELPER_FUNCTIONS_H
-#define HELPER_FUNCTIONS_H
-
-int Check_collision(SDL_Rect rect, SDL_Rect ball);
-int Gameover(SDL_Renderer* renderer, TTF_Font* font);
-
-#endif
